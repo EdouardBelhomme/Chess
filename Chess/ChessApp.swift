feb0d8282a6ab34d95aa62e16f0d83b9f79ed76b
Edouard Belhomme <edouard.belhomme@epitech.eu>
2025-12-15 14:00:57 +0100

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Edouard Belhomme on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
